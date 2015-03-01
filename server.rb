935a0a84bb5c43b696fc7674971d0212586fb6b3
pderichs <patderichs@gmail.com>
2015-03-01 13:16:40 +0100

    Fixed rubocop issues

--- server.rb
+++ server.rb
@@ -9,27 +9,29 @@ exit if ARGV.size != 1
 
 config = YAML.load_file(ARGV[0])
 
-# Exchange your oauth_token and oauth_token_secret for an AccessToken instance.
-def prepare_access_token(config)
-  consumer = OAuth::Consumer.new(
+def twitter_consumer_by(config)
+  OAuth::Consumer.new(
     config['consumer_key'],
     config['consumer_secret'],
-    {
-      :site => "https://api.twitter.com",
-      scheme: :header
-    }
+    site: 'https://api.twitter.com',
+    scheme: :header
   )
+end
 
-  # now create the access token object from passed values
-  token_hash = {
+def token_hash_by(config)
+  {
     oauth_token: config['access_token'],
     oauth_token_secret: config['access_token_secret']
   }
+end
 
+def prepare_access_token(config)
+  consumer = twitter_consumer_by(config)
+  token_hash = token_hash_by(config)
   OAuth::AccessToken.from_hash(consumer, token_hash)
 end
 
-# Exchange our oauth_token and oauth_token secret for the AccessToken instance.
+# now create the access token object from passed values
 access_token = prepare_access_token(config)
 
 get '/' do
